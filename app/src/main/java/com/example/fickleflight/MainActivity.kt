fe2fb5b4b956978389d74b839612e7bd2334ac86
Ivan Flores <ifloresmanc96@gmail.com>
2024-05-28 22:43:55 -0300

    try to nav grah- Fragments errors

--- app/src/main/java/com/example/fickleflight/MainActivity.kt
+++ app/src/main/java/com/example/fickleflight/MainActivity.kt
@@ -5,8 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
