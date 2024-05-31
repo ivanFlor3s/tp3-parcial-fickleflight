d3dafae106dbace2815dfe591846042ac8caaad7
Ivan Flores <ifloresmanc96@gmail.com>
2024-05-31 00:38:57 -0300

    add splash and changes in activities use main activity for splash + use app activity for application

--- app/src/main/java/com/example/fickleflight/AppActivity.kt
+++ app/src/main/java/com/example/fickleflight/AppActivity.kt
@@ -1,30 +1,16 @@
 package com.example.fickleflight
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        supportActionBar?.hide()
-
-        // Otra forma de hacer splashscreen con un theme y sleep
-        // https://youtu.be/ksaaMt8Lo6U?t=540
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this@MainActivity, AppActivity::class.java)
-            startActivity(intent)
-        }, 3000)
-
+        setContentView(R.layout.activity_app)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
