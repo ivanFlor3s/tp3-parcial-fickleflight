b58783ef24df3382e746c54f1b94d635c322db9d
Ivan Flores <ifloresmanc96@gmail.com>
2024-06-01 18:00:13 -0300

    custom toolbar material try

--- app/src/main/java/com/example/fickleflight/AppActivity.kt
+++ app/src/main/java/com/example/fickleflight/AppActivity.kt
@@ -5,24 +5,49 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.fickleflight.databinding.ActivityAppBinding
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AppActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     private lateinit var binding : ActivityAppBinding
+
+    private val fragmentsNavigation = setOf(
+        R.id.navigation_profile,
+        R.id.navigation_calendar,
+        R.id.navigation_preference,
+        R.id.navigation_get_help,
+        R.id.navigation_notification,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityAppBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_app)
+
+        setContentView(binding.root)
+
+        navController = findNavController(R.id.fragment)
+
+        setSupportActionBar(binding.customToolbar)
+
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = fragmentsNavigation,
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
 
 
-        setSupportActionBar(findViewById(R.id.custom_toolbar))
+        binding.customToolbar.setupWithNavController(navController,appBarConfiguration)
+
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navController = findNavController(R.id.fragment)
 
         bottomNavigationView.setupWithNavController(navController)
 
