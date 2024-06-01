771ac03833bae24ac9d97328a35c77c76ddd3d1b
Valentin Ignacio Arce <127315229+valentinArce@users.noreply.github.com>
2024-06-01 19:54:16 -0300

    Fragment Settings version2

--- app/src/main/java/com/example/fickleflight/AppActivity.kt
+++ app/src/main/java/com/example/fickleflight/AppActivity.kt
@@ -1,6 +1,7 @@
 package com.example.fickleflight
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,10 +11,20 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AppActivity : AppCompatActivity() {
+
+    private val PREF_NAME = "settings"
+    private val DARK_MODE = "dark_mode"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_app)
+
+        val sharedPreferences = getSharedPreferences(PREF_NAME, 0)
+         if (sharedPreferences.getBoolean(DARK_MODE, false)) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES) //cambia el modo de la app.
+         } else {
+             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+         }
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         val navController = findNavController(R.id.fragment)
