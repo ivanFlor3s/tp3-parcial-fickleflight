ff1b25553a61ede33eb8ee8ac2ddedbac59a1cc1
Ivan Flores <ifloresmanc96@gmail.com>
2024-06-01 23:10:30 -0300

    add icon gone in toolbar + show icon in toolbar by destination

--- app/src/main/java/com/example/fickleflight/AppActivity.kt
+++ app/src/main/java/com/example/fickleflight/AppActivity.kt
@@ -3,9 +3,11 @@ package com.example.fickleflight
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.marginTop
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -63,13 +65,17 @@ class AppActivity : AppCompatActivity() {
                 R.id.exploreFragment
                 -> {
                     binding.contentMainInclude.toolbar.visibility = MaterialToolbar.VISIBLE
-                    binding.contentMainInclude.toolbarTitle.text = getNameByDestination(destination)
+                    binding.contentMainInclude.toolbarTitle.visibility = View.GONE
+                    binding.contentMainInclude.appIcon.visibility = View.VISIBLE
+                    binding.contentMainInclude.appIcon.setImageResource(R.drawable.logo)
                     binding.contentMainInclude.leftIcon.setImageResource(R.drawable.baseline_menu_24)
                     binding.contentMainInclude.rightIcon.setImageResource(R.drawable.profile_notification)
                 }
 
                 R.id.searchFragment -> {
                     binding.contentMainInclude.toolbar.visibility = MaterialToolbar.VISIBLE
+                    binding.contentMainInclude.toolbarTitle.visibility = View.VISIBLE
+                    binding.contentMainInclude.appIcon.visibility = View.GONE
                     binding.contentMainInclude.toolbarTitle.text = getNameByDestination(destination)
                     binding.contentMainInclude.leftIcon.setImageResource(R.drawable.outline_keyboard_backspace_24)
                     binding.contentMainInclude.rightIcon.setImageResource(R.drawable.outline_more_vert_24)
@@ -77,6 +83,8 @@ class AppActivity : AppCompatActivity() {
 
                 R.id.offersFragment -> {
                     binding.contentMainInclude.toolbar.visibility = MaterialToolbar.VISIBLE
+                    binding.contentMainInclude.toolbarTitle.visibility = View.VISIBLE
+                    binding.contentMainInclude.appIcon.visibility = View.GONE
                     binding.contentMainInclude.toolbarTitle.text = getNameByDestination(destination)
                     binding.contentMainInclude.leftIcon.setImageResource(R.drawable.outline_keyboard_backspace_24)
                     binding.contentMainInclude.rightIcon.visibility = View.GONE
@@ -86,11 +94,16 @@ class AppActivity : AppCompatActivity() {
                 }
                 R.id.settingsFragment -> {
                     binding.contentMainInclude.toolbar.visibility = MaterialToolbar.VISIBLE
+                    binding.contentMainInclude.toolbarTitle.visibility = View.GONE
+                    binding.contentMainInclude.appIcon.visibility = View.VISIBLE
+                    binding.contentMainInclude.appIcon.setImageResource(R.drawable.logo)
                     binding.contentMainInclude.toolbarTitle.text = "FlickleFlight"
                     binding.contentMainInclude.leftIcon.setImageResource(R.drawable.outline_keyboard_backspace_24)
                     binding.contentMainInclude.rightIcon.visibility = View.GONE
                 }
                 R.id.resulstsFragment -> {
+                    binding.contentMainInclude.toolbarTitle.visibility = View.VISIBLE
+                    binding.contentMainInclude.appIcon.visibility = View.GONE
                     binding.contentMainInclude.toolbar.visibility = MaterialToolbar.VISIBLE
                     binding.contentMainInclude.toolbarTitle.text = "EZE -> LAX"
                     binding.contentMainInclude.leftIcon.setImageResource(R.drawable.outline_keyboard_backspace_24)
