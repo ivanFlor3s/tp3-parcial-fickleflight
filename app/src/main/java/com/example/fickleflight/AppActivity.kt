06c54fd279f684966075de78b28a3ce157c9112e
Ivan Flores <ifloresmanc96@gmail.com>
2024-06-01 22:12:10 -0300

    custom toolbar option

--- app/src/main/java/com/example/fickleflight/AppActivity.kt
+++ app/src/main/java/com/example/fickleflight/AppActivity.kt
@@ -1,11 +1,14 @@
 package com.example.fickleflight
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -20,11 +23,13 @@ class AppActivity : AppCompatActivity() {
     private lateinit var binding : ActivityAppBinding
 
     private val fragmentsNavigation = setOf(
-        R.id.navigation_profile,
-        R.id.navigation_calendar,
-        R.id.navigation_preference,
-        R.id.navigation_get_help,
-        R.id.navigation_notification,
+        R.id.exploreFragment,
+        R.id.detailFragment,
+        R.id.offersFragment,
+        R.id.profileFragment,
+        R.id.searchFragment,
+        R.id.settingsFragment,
+        R.id.resulstsFragment
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,25 +41,81 @@ class AppActivity : AppCompatActivity() {
 
         navController = findNavController(R.id.fragment)
 
-        setSupportActionBar(binding.customToolbar)
-
-        val appBarConfiguration = AppBarConfiguration(
-            topLevelDestinationIds = fragmentsNavigation,
-            fallbackOnNavigateUpListener = ::onSupportNavigateUp
-        )
-
-
-        binding.customToolbar.setupWithNavController(navController,appBarConfiguration)
-
+        //binding.contentMainInclude.toolbar.setupWithNavController(navController, AppBarConfiguration(fragmentsNavigation))
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-
         bottomNavigationView.setupWithNavController(navController)
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //Configuro Comportamiento segun Fragmento
+        navController.addOnDestinationChangedListener { _, destination: NavDestination, _ ->
+
+            //Esto es para configurar el Icono Fijo
+            //supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_bar_chart_black_24dp)
+
+            when (destination.id) {
+                R.id.exploreFragment
+                -> {
+                    binding.contentMainInclude.toolbar.visibility = MaterialToolbar.VISIBLE
+                    binding.contentMainInclude.toolbarTitle.text = getNameByDestination(destination)
+                    binding.contentMainInclude.leftIcon.setImageResource(R.drawable.baseline_menu_24)
+                    binding.contentMainInclude.rightIcon.setImageResource(R.drawable.profile_notification)
+                }
+
+                R.id.searchFragment -> {
+                    binding.contentMainInclude.toolbar.visibility = MaterialToolbar.VISIBLE
+                    binding.contentMainInclude.toolbarTitle.text = getNameByDestination(destination)
+                    binding.contentMainInclude.leftIcon.setImageResource(R.drawable.outline_keyboard_backspace_24)
+                    binding.contentMainInclude.rightIcon.setImageResource(R.drawable.outline_more_vert_24)
+                }
+
+                R.id.offersFragment -> {
+                    binding.contentMainInclude.toolbar.visibility = MaterialToolbar.VISIBLE
+                    binding.contentMainInclude.toolbarTitle.text = getNameByDestination(destination)
+                    binding.contentMainInclude.leftIcon.setImageResource(R.drawable.outline_keyboard_backspace_24)
+                    binding.contentMainInclude.rightIcon.visibility = View.GONE
+                }
+                R.id.profileFragment -> {
+                    binding.contentMainInclude.toolbar.visibility = MaterialToolbar.GONE
+                }
+                R.id.settingsFragment -> {
+                    binding.contentMainInclude.toolbar.visibility = MaterialToolbar.VISIBLE
+                    binding.contentMainInclude.toolbarTitle.text = "FlickleFlight"
+                    binding.contentMainInclude.leftIcon.setImageResource(R.drawable.outline_keyboard_backspace_24)
+                    binding.contentMainInclude.rightIcon.visibility = View.GONE
+                }
+                R.id.resulstsFragment -> {
+                    binding.contentMainInclude.toolbar.visibility = MaterialToolbar.VISIBLE
+                    binding.contentMainInclude.toolbarTitle.text = "EZE -> LAX"
+                    binding.contentMainInclude.leftIcon.setImageResource(R.drawable.outline_keyboard_backspace_24)
+                    binding.contentMainInclude.rightIcon.visibility = View.GONE
+                }
+                R.id.detailFragment -> {
+                    binding.contentMainInclude.toolbar.visibility = MaterialToolbar.GONE
+                }
+            }
+        }
+
+
+
+    }
+
+    private fun getNameByDestination( destination: NavDestination): String {
+        return when (destination.id) {
+            R.id.exploreFragment -> "Explore"
+            R.id.detailFragment -> "Detail"
+            R.id.offersFragment -> "Offers"
+            R.id.profileFragment -> "Profile"
+            R.id.searchFragment -> "Search"
+            R.id.settingsFragment -> "Settings"
+            R.id.resulstsFragment -> "Results"
+            else -> "Unknown"
         }
     }
 }
